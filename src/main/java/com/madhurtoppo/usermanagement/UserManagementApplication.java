2ef8d70b9a6c702a854b277a2e5340d4408956d9
Madhur Toppo <madhur.toppo@gmail.com>
2023-07-02 01:30:06 +0530

    Rename Spring application name

--- src/main/java/com/madhurtoppo/usermanagement/UserManagementApplication.java
+++ src/main/java/com/madhurtoppo/usermanagement/UserManagementApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class UserServiceApplication {
+public class UserManagementApplication {
 
   public static void main(String[] args) {
-    SpringApplication.run(UserServiceApplication.class, args);
+    SpringApplication.run(UserManagementApplication.class, args);
   }
 }
