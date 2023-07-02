9f799c52a93e43f1dcfda932217e13f14a96bc6d
Madhur Toppo <madhur.toppo@gmail.com>
2023-07-02 19:47:33 +0530

    Change UsersDto to Record type

--- src/main/java/com/madhurtoppo/usermanagement/dtos/UsersDto.java
+++ src/main/java/com/madhurtoppo/usermanagement/dtos/UsersDto.java
@@ -1,14 +1,6 @@
 package com.madhurtoppo.usermanagement.dtos;
 
 import java.util.List;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
 
 /** UsersDto */
-@Getter
-@Setter
-@AllArgsConstructor
-public class UsersDto {
-  private List<UserDto> users;
-}
+public record UsersDto(List<UserDto> users) {}
