16a868c7d91037919a44b9c441fec0750dfe1d71
Madhur Toppo <madhur.toppo@gmail.com>
2023-07-20 00:27:19 +0530

    Rename Error boolean field to success

--- src/main/java/com/madhurtoppo/usermanagement/exceptions/Error.java
+++ src/main/java/com/madhurtoppo/usermanagement/exceptions/Error.java
@@ -3,5 +3,5 @@ package com.madhurtoppo.usermanagement.exceptions;
 import java.util.Date;
 
 /** ErrorDetails */
-public record Error(boolean status, String message, String details, Date timestamp) {
+public record Error(boolean success, String message, String details, Date timestamp) {
 }
